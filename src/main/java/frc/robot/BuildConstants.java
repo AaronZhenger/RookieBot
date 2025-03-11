dfe7219396a5b970a0b9e108ab6297e6fa53493a
Aaron-FTC-Junior <alock8mini@gmail.com>
2025-03-11 15:05:35 -0500

    idk

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RookieBot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "39162e68ded9462bd1c099c5b6b1f6cff69393d1";
-  public static final String GIT_DATE = "2025-03-04 18:53:08 CST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "98615283848349deadfd8c35edc1f65c6ba90f33";
+  public static final String GIT_DATE = "2025-03-05 19:06:13 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-04 21:13:13 CST";
-  public static final long BUILD_UNIX_TIME = 1741144393303L;
+  public static final String BUILD_DATE = "2025-03-08 20:25:06 CST";
+  public static final long BUILD_UNIX_TIME = 1741487106017L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/kitbot/frc/robot/Subsystems/DriveSubsystem/DriveIOTalon.java
+++ src/main/java/kitbot/frc/robot/Subsystems/DriveSubsystem/DriveIOTalon.java
@@ -1,6 +1,10 @@
 package kitbot.frc.robot.Subsystems.DriveSubsystem;
 
+import static edu.wpi.first.units.Units.Degrees;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
@@ -12,45 +16,50 @@ import kitbot.lib.motors.real.SteelTalonFX;
 import kitbot.lib.motors.real.MotorConfiguration.IdleState;
 import kitbot.lib.motors.real.MotorConfiguration.MotorMode;
 
+//TODO: Rework File (with CTRE motors not teamlib)
 public class DriveIOTalon implements DriveIO {
 
-    private SteelTalonFX left = new SteelTalonFX(DriveConstants.kLeftFrontID);
-    private SteelTalonFX right = new SteelTalonFX(DriveConstants.kRightFrontID);
+    private TalonFX left1 = new TalonFX(DriveConstants.kLeftFrontID.getDeviceNumber());
+    private TalonFX left2 = new TalonFX(DriveConstants.kLeftBackID.getDeviceNumber());
+    private TalonFX right1 = new TalonFX(DriveConstants.kRightFrontID.getDeviceNumber());
+    private TalonFX right2 = new TalonFX(DriveConstants.kRightBackID.getDeviceNumber());
 
-    private MotorConfiguration driveConfig = new MotorConfiguration();
+    private TalonFXConfiguration leftConfig = new TalonFXConfiguration();
+    private TalonFXConfiguration rightConfig = new TalonFXConfiguration(); 
 
-    private Pigeon2 gyro = new Pigeon2(DriveConstants.kPigeonID);
+    // private Pigeon2 gyro = new Pigeon2(DriveConstants.kPigeonID);
 
     public DriveIOTalon() {
-        driveConfig.idleState = IdleState.kBrake;
-        driveConfig.gearRatio = new ComplexGearRatio(DriveConstants.kDriveGearRatio);
-        driveConfig.mode = MotorMode.kFlywheel;
-        driveConfig.currentLimit = 60;
-        driveConfig.finalDiameterMeters = Units.inchesToMeters(DriveConstants.kWheelDiameter);
-        driveConfig.maxVelocity = driveConfig.getStandardMaxVelocity(5676);
-        driveConfig.maxAcceleration = driveConfig.getStandardMaxVelocity(5676)/3.0;
-        driveConfig.kP = DriveConstants.kP;
-        driveConfig.kI = DriveConstants.kI;
-        driveConfig.kD = DriveConstants.kD;
-        driveConfig.kV = DriveConstants.kMaxVelocity;
-        driveConfig.kA = DriveConstants.kMaxAcceleration;
+        // driveConfig.idleState = IdleState.kBrake;
+        // driveConfig.gearRatio = new ComplexGearRatio(DriveConstants.kDriveGearRatio);
+        // driveConfig.mode = MotorMode.kFlywheel;
+        // driveConfig.currentLimit = 60;
+        // driveConfig.finalDiameterMeters = Units.inchesToMeters(DriveConstants.kWheelDiameter);
+        // driveConfig.maxVelocity = driveConfig.getStandardMaxVelocity(5676);
+        // driveConfig.maxAcceleration = driveConfig.getStandardMaxVelocity(5676)/3.0;
+        // driveConfig.kP = DriveConstants.kP;
+        // driveConfig.kI = DriveConstants.kI;
+        // driveConfig.kD = DriveConstants.kD;
+        // driveConfig.kV = DriveConstants.kMaxVelocity;
+        // driveConfig.kA = DriveConstants.kMaxAcceleration;
 
-        left.apply(driveConfig);
-        right.apply(driveConfig);
+        // left.apply(driveConfig);
+        // right.apply(driveConfig);
     }
 
     @Override
     public void updateInputs(DriveIOInputsAutoLogged inputs) {
-        inputs.leftSpeed = left.getEncoderVelocity();
-        inputs.rightSpeed = right.getEncoderVelocity();
-        inputs.leftDistanceTravelled = left.getEncoderPosition()/1;
-        inputs.rightDistanceTravelled = right.getEncoderPosition()/1;
+        // inputs.leftSpeed = left.getEncoderVelocity();
+        // inputs.rightSpeed = right.getEncoderVelocity();
+        // inputs.leftDistanceTravelled = left.getEncoderPosition()/1;
+        // inputs.rightDistanceTravelled = right.getEncoderPosition()/1;
     }
 
 
     
     public Angle getGyroRotation() {
-        return gyro.getYaw().getValue();
+        // return gyro.getYaw().getValue();
+        return Angle.ofBaseUnits(0, Degrees);
     }
 
     @Override
